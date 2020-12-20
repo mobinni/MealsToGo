b1a379debc7981abbdde44c34571299615d9b03b
education <education@MacBook-Pro.local>
2020-12-20 13:29:13 -0500

    Checkpoint 5 feature setup for restaurant info component part 2

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,4 +1,18 @@
 import React from "react";
 import { Text } from "react-native";
 
-export const RestaurantInfo = () => <Text>Restaurant Info </Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return <Text>{name}</Text>;
+};
