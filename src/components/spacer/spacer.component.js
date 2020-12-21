22a8f02a6f0f4f41b6225d17a3f8d3ab4d2a141c
education <education@MacBook-Pro.local>
2020-12-21 16:34:26 -0500

    Checkpoint spacer optimization

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,45 +1,31 @@
-import React from "react";
 import styled from "styled-components/native";
 
-const TopSmall = styled.View`
-  margin-top: ${(props) => props.theme.space[1]};
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
+
+const positionVariant = {
+  top: "marginTop",
+  left: "marginLeft",
+  right: "marginRight",
+  bottom: "marginBottom",
+};
+
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[position];
+  const value = theme.space[sizeIndex];
+
+  return `${property}:${value}`;
+};
+
+export const Spacer = styled.View`
+  ${({ position, size, theme }) => getVariant(position, size, theme)}
 `;
 
-const TopMedium = styled.View`
-  margin-top: ${(props) => props.theme.space[2]};
-`;
-
-const TopLarge = styled.View`
-  margin-top: ${(props) => props.theme.space[3]};
-`;
-
-const LeftSmall = styled.View`
-  margin-left: ${(props) => props.theme.space[1]};
-`;
-
-const LeftMedium = styled.View`
-  margin-left: ${(props) => props.theme.space[2]};
-`;
-
-const LeftLarge = styled.View`
-  margin-left: ${(props) => props.theme.space[3]};
-`;
-
-export const Spacer = ({ variant }) => {
-  if (variant === "top.medium") {
-    return <TopMedium />;
-  }
-  if (variant === "top.large") {
-    return <TopLarge />;
-  }
-  if (variant === "left.small") {
-    return <LeftSmall />;
-  }
-  if (variant === "left.medium") {
-    return <LeftMedium />;
-  }
-  if (variant === "left.large") {
-    return <LeftLarge />;
-  }
-  return <TopSmall />;
+Spacer.defaultProps = {
+  position: "top",
+  size: "small",
 };
