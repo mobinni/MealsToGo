e960481f61ebca6a306cc12a14a9af64c7473c4a
education <education@MacBook-Pro.local>
2020-12-22 13:37:46 -0500

    Checkpoint context hookup

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,14 +1,42 @@
 import React, { useState, createContext, useEffect, useMemo } from "react";
 
-import { restaurantRequest, restaurantTransform } from "./restaurants.service";
+import {
+  restaurantsRequest,
+  restaurantsTransform,
+} from "./restaurants.service";
 
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+        .then(restaurantsTransform)
+        .then((results) => {
+          setIsLoading(false);
+          setRestaurants(results);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err);
+        });
+    }, 2000);
+  };
+  useEffect(() => {
+    retrieveRestaurants();
+  }, []);
+
   return (
     <RestaurantsContext.Provider
       value={{
-        restaurants: [1, 2, 3, 4, 5, 6, 7, 8],
+        restaurants,
+        isLoading,
+        error,
       }}
     >
       {children}
