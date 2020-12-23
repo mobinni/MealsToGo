bc7783982565aa03eefd14bfe33eae07bd4ff699
education <education@MacBook-Pro.local>
2020-12-23 11:37:47 -0500

    Checkpoint restaurant detail navigation

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "react-native";
 
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -12,6 +13,10 @@ export const RestaurantsNavigator = () => {
       <RestaurantStack.Screen
         name="Restaurants"
         component={RestaurantsScreen}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={() => <Text>Restaurant Detail</Text>}
       />
     </RestaurantStack.Navigator>
   );
