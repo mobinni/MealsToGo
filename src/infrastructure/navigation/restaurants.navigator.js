06bb0af171c193bb1a33822ffdf62e196a1a4089
Mo Binni <mo@binni.io>
2020-12-23 14:01:32 -0500

    Revert "42 detail sreen card"

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "react-native";
 
 import {
   createStackNavigator,
@@ -6,7 +7,6 @@ import {
 } from "@react-navigation/stack";
 
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
-import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
 
 const RestaurantStack = createStackNavigator();
 
@@ -24,7 +24,7 @@ export const RestaurantsNavigator = () => {
       />
       <RestaurantStack.Screen
         name="RestaurantDetail"
-        component={RestaurantDetailScreen}
+        component={() => <Text>Restaurant Detail</Text>}
       />
     </RestaurantStack.Navigator>
   );
