0ce87030378e11e6a5ce55abd1d71b426cf0eff0
education <education@MacBook-Pro.local>
2020-12-23 17:18:24 -0500

    Checkpoint mapview with search

---
+++ src/features/map/components/search.component.js
@@ -0,0 +1,36 @@
+import React, { useContext, useState, useEffect } from "react";
+import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+
+import { LocationContext } from "../../../services/location/location.context";
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
+`;
+
+export const Search = () => {
+  const { keyword, search } = useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
+  useEffect(() => {
+    setSearchKeyword(keyword);
+  }, [keyword]);
+  return (
+    <SearchContainer>
+      <Searchbar
+        placeholder="Search for a location"
+        icon="map"
+        value={searchKeyword}
+        onSubmitEditing={() => {
+          search(searchKeyword);
+        }}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+      />
+    </SearchContainer>
+  );
+};
