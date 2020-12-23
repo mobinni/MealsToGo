48bf4ba9345f822b6776e9f35c1f645e2a7e21c4
education <education@MacBook-Pro.local>
2020-12-23 17:05:46 -0500

    Checkpoint maps Setup

---
+++ src/features/map/screens/map.screen.js
@@ -0,0 +1,4 @@
+import React, { useContext, useState, useEffect } from "react";
+import MapView from "react-native-maps";
+
+export const MapScreen = () => <MapView style={{ height: "100%" }} />;
--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -7,6 +7,7 @@ import { Text } from "react-native";
 import { SafeArea } from "../../components/utility/safe-area.component";
 
 import { RestaurantsNavigator } from "./restaurants.navigator";
+import { MapScreen } from "../../features/map/screens/map.screen";
 
 const Tab = createBottomTabNavigator();
 
@@ -19,11 +20,6 @@ const TAB_ICON = {
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
-  </SafeArea>
-);
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
   </SafeArea>
 );
 
@@ -46,7 +42,7 @@ export const AppNavigator = () => (
       }}
     >
       <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Map" component={MapScreen} />
       <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
   </NavigationContainer>
