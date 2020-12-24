657898fe6a2b6b12278cbaec067dd830700db313
education <education@MacBook-Pro.local>
2020-12-24 01:12:31 -0500

    Checkpoint favourites bar part 2

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -1,3 +1,41 @@
 import React from "react";
-import { Text } from "react-native";
-export const FavouritesBar = () => <Text>Favourites Bar</Text>;
+import { ScrollView, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { Spacer } from "../spacer/spacer.component";
+import { CompactRestaurantInfo } from "../restaurant/compact-restaurant-info.component";
+import { Text } from "../typography/text.component";
+
+const FavouritesWrapper = styled.View`
+  padding: 10px;
+`;
+export const FavouritesBar = ({ favourites, onNavigate }) => {
+  if (!favourites.length) {
+    return null;
+  }
+  return (
+    <FavouritesWrapper>
+      <Spacer variant="left.large">
+        <Text variant="caption">Favourites</Text>
+      </Spacer>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {favourites.map((restaurant) => {
+          const key = restaurant.name;
+          return (
+            <Spacer key={key} position="left" size="medium">
+              <TouchableOpacity
+                onPress={() =>
+                  onNavigate("RestaurantDetail", {
+                    restaurant,
+                  })
+                }
+              >
+                <CompactRestaurantInfo restaurant={restaurant} />
+              </TouchableOpacity>
+            </Spacer>
+          );
+        })}
+      </ScrollView>
+    </FavouritesWrapper>
+  );
+};
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -8,6 +8,7 @@ import { Spacer } from "../../../components/spacer/spacer.component";
 import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { FavouritesContext } from "../../../services/favourites/favourites.context";
 
 import { Search } from "../components/search.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
@@ -29,6 +30,7 @@ const LoadingContainer = styled.View`
 
 export const RestaurantsScreen = ({ navigation }) => {
   const { isLoading, restaurants } = useContext(RestaurantsContext);
+  const { favourites } = useContext(FavouritesContext);
   const [isToggled, setIsToggled] = useState(false);
 
   return (
@@ -42,7 +44,9 @@ export const RestaurantsScreen = ({ navigation }) => {
         isFavouritesToggled={isToggled}
         onFavouritesToggle={() => setIsToggled(!isToggled)}
       />
-      {isToggled && <FavouritesBar />}
+      {isToggled && (
+        <FavouritesBar favourites={favourites} onNavigate={navigation.navigate} />
+      )}
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
