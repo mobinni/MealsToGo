fb82e63befb7f2b18855e3c168cb1ea6d7457520
education <education@MacBook-Pro.local>
2020-12-24 11:19:09 -0500

    Checkpoint auth test

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -25,7 +25,6 @@ export const LocationContextProvider = ({ children }) => {
       .then((result) => {
         setIsLoading(false);
         setLocation(result);
-        console.log(result);
       })
       .catch((err) => {
         setIsLoading(false);
