cf87315323a6ae2172579d2e5b9cd33bf9792255
education <education@MacBook-Pro.local>
2020-12-24 01:38:48 -0500

    checkpoint android bug

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -25,8 +25,8 @@ const Item = styled.View`
 
 const isAndroid = Platform.OS === "android";
 
-export const CompactRestaurantInfo = ({ restaurant }) => {
-  const Image = isAndroid ? CompactWebview : CompactImage;
+export const CompactRestaurantInfo = ({ restaurant, isMap }) => {
+  const Image = isAndroid && isMap ? CompactWebview : CompactImage;
 
   return (
     <Item>
--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -3,5 +3,5 @@ import React from "react";
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
 
 export const MapCallout = ({ restaurant }) => (
-  <CompactRestaurantInfo restaurant={restaurant} />
+  <CompactRestaurantInfo isMap restaurant={restaurant} />
 );
