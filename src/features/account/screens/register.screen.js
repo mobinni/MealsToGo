281a456e22b9c2d4c063bcb7c0189e92305f4c58
education <education@MacBook-Pro.local>
2020-12-26 12:14:44 -0500

    Checkpoint background cover

--- src/features/account/screens/register.screen.js
+++ src/features/account/screens/register.screen.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-import { AccountBackground } from "../components/account.styles";
+import { AccountBackground, AccountCover } from "../components/account.styles";
 
 export const RegisterScreen = () => {
-  return <AccountBackground />;
+  return (
+    <AccountBackground>
+      <AccountCover />
+    </AccountBackground>
+  );
 };
