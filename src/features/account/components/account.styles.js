af7864b416ce46507d6a832330655c38fe51b18a
education <education@MacBook-Pro.local>
2020-12-26 19:16:09 -0500

    Checkpoint lighten

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -7,3 +7,10 @@ export const AccountBackground = styled.ImageBackground.attrs({
   align-items: center;
   justify-content: center;
 `;
+
+export const AccountCover = styled.View`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.3);
+`;
