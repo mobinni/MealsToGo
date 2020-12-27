65012c3fdb8c3a14b39f91129ea148cbccd44f50
education <education@MacBook-Pro.local>
2020-12-26 19:57:09 -0500

    Checkpoint login screen exercise

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,4 +1,4 @@
 import * as firebase from "firebase";
 
-const loginRequest = (email, password) =>
+export const loginRequest = (email, password) =>
   firebase.auth().signInWithEmailAndPassword(email, password);
