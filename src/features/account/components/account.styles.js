577277171e5b8f83575ba0ccc573449e216fd427
education <education@MacBook-Pro.local>
2020-12-26 19:57:52 -0500

    Checkpoint solution login

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { Button } from "react-native-paper";
+import { Button, TextInput } from "react-native-paper";
 import { colors } from "../../../infrastructure/theme/colors";
 
 export const AccountBackground = styled.ImageBackground.attrs({
@@ -27,4 +27,8 @@ export const AuthButton = styled(Button).attrs({
   color: colors.brand.primary,
 })`
   padding: ${(props) => props.theme.space[2]};
+`;
+
+export const AuthInput = styled(TextInput)`
+  width: 300px;
 `;
