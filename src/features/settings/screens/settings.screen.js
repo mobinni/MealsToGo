53b3f9050932e9dd0cb3762cb9e69b0b26f3af66
education <education@MacBook-Pro.local>
2020-12-26 23:44:59 -0500

    Checkpoint settings

---
+++ src/features/settings/screens/settings.screen.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+
+import { List } from "react-native-paper";
+
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
+export const SettingsScreen = ({ navigation }) => {
+  const { onLogout } = useContext(AuthenticationContext);
+  return (
+    <SafeArea>
+      <List.Section>
+        <List.Item
+          style={{ padding: 16 }}
+          title="Favourites"
+          description="View your favourites"
+          left={(props) => <List.Icon {...props} color="black" icon="heart" />}
+          onPress={() => navigation.navigate("Favourites")}
+        />
+        <List.Item
+          style={{ padding: 16 }}
+          title="Logout"
+          left={(props) => <List.Icon {...props} color="black" icon="door" />}
+          onPress={onLogout}
+        />
+      </List.Section>
+    </SafeArea>
+  );
+};
