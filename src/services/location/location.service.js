10b06d59cb129dc9d93b2edbf8e2652cbb12991b
education <education@MacBook-Pro.local>
2020-12-29 19:13:41 -0500

    Checkpoint Move Mock Part 2

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,8 +1,15 @@
 import camelize from "camelize";
 
-export const locationRequest = (searchTerm) => {};
+export const locationRequest = (searchTerm) => {
+  return fetch(
+    `http://localhost:5001/mealstogo-b2612/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
+  });
+};
 
 export const locationTransform = (result) => {
+  console.log(result)
   const formattedResponse = camelize(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
