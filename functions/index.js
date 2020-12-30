9dd667d7226f2142fdbf915958e41eadf2e31d34
education <education@MacBook-Pro.local>
2020-12-29 23:46:05 -0500

    Checkpoint Move Mock Restaurants

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const functions = require("firebase-functions");
 const { geocodeRequest } = require("./geocode");
+const { placesRequest } = require("./places");
 
 exports.geocode = functions.https.onRequest((request, response) => {
   geocodeRequest(request, response);
+});
+
+exports.placesNearby = functions.https.onRequest((request, response) => {
+  placesRequest(request, response);
 });
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,22 +1,15 @@
-import { mocks, mockImages } from "./mock";
 import camelize from "camelize";
 
 export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject("not found");
-    }
-    resolve(mock);
+  return fetch(
+    `http://localhost:5001/mealstogo-b2612/us-central1/placesNearby?location=${location}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
-
     return {
       ...restaurant,
       address: restaurant.vicinity,
