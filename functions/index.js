8e3530682599863065b2b0e4d2849f9081f0eca4
education <education@MacBook-Pro.local>
2020-12-29 19:08:26 -0500

    Checkpoint Move Mock Part 1

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,6 @@
 const functions = require("firebase-functions");
+const { geocodeRequest } = require("./geocode");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebaseeeeeeeeeeeeeeeee!");
+exports.geocode = functions.https.onRequest((request, response) => {
+  geocodeRequest(request, response);
 });
