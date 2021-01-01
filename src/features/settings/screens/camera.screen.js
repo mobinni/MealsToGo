407aa219e8cefd96dca012f1aabe88d19e0f910b
education <education@MacBook-Pro.local>
2021-01-01 14:51:26 -0500

    Checkpoint take a picture

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Camera } from "expo-camera";
 import styled from "styled-components/native";
-import { View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 import { Text } from "../../../components/typography/text.component";
 
 const ProfileCamera = styled(Camera)`
@@ -12,6 +12,13 @@ const ProfileCamera = styled(Camera)`
 export const CameraScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -27,9 +34,11 @@ export const CameraScreen = () => {
     return <Text>No access to camera</Text>;
   }
   return (
-    <ProfileCamera
-      ref={(camera) => (cameraRef.current = camera)}
-      type={Camera.Constants.Type.front}
-    ></ProfileCamera>
+    <TouchableOpacity onPress={snap}>
+      <ProfileCamera
+        ref={(camera) => (cameraRef.current = camera)}
+        type={Camera.Constants.Type.front}
+      />
+    </TouchableOpacity>
   );
 };
