bba73910d8cd7aa10cdbbfc4fcfb6a6575508d08
education <education@MacBook-Pro.local>
2021-01-15 18:38:50 -0500

    Checkpoint: Credit Card Token

--- src/features/checkout/components/credit-card.component.js
+++ src/features/checkout/components/credit-card.component.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { LiteCreditCardInput } from "react-native-credit-card-input";
+import createStripe from "stripe-client";
+
+const stripe = createStripe(
+  "pk_test_51HzDwlIfEuMDL6nvAXXM3pbywxTyQjerlEGqAyg8sUESBJKtAy6j86uMoCvYLInPpahyRSC3S8G65md8jbkGYPNE002bZm2QZf"
+);
 
 export const CreditCardInput = () => {
-  const onChange = (formData) => {
+  const onChange = async (formData) => {
     const { values, status } = formData;
     const isIncomplete = Object.values(status).includes("incomplete");
     console.log(isIncomplete);
+    const card = {
+      number: "4242424242424242",
+      exp_month: "02",
+      exp_year: "24",
+      cvc: "244",
+      name: "Mo",
+    };
+    const info = await stripe.createToken({ card });
+    console.log(info);
   };
   return <LiteCreditCardInput onChange={onChange} />;
 };
