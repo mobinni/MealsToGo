cc37a809037652182bd30b77e1f8ccce050c2a7a
education <education@MacBook-Pro.local>
2021-01-15 19:20:22 -0500

    Checkpoint: Checkout Cart Context

---
+++ src/services/cart/cart.context.js
@@ -0,0 +1,39 @@
+import React, { createContext, useState, useEffect, useContext } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { AuthenticationContext } from "../authentication/authentication.context";
+
+export const CartContext = createContext();
+
+export const CartContextProvider = ({ children }) => {
+  const { user } = useContext(AuthenticationContext);
+
+  const [cart, setCart] = useState([]);
+  const [restaurant, setRestaurant] = useState(null);
+
+  const add = (item, rst) => {
+    if (!restaurant || restaurant.placeId !== rst.placeId) {
+      setRestaurant(rst);
+      setCart([item]);
+    }
+    setCart([...cart, item]);
+  };
+
+  const clear = () => {
+    setCart([]);
+    setRestaurant(null);
+  };
+
+  return (
+    <CartContext.Provider
+      values={{
+        addToCart: add,
+        clearCart: clear,
+        restaurant,
+        cart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
