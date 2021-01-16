c9a86f4701185c954df87d6f1768303ec5833df8
education <education@MacBook-Pro.local>
2021-01-15 20:12:45 -0500

    Checkpoint Empty Cart

--- src/features/checkout/screens/checkout.screen.js
+++ src/features/checkout/screens/checkout.screen.js
@@ -1,14 +1,27 @@
 import React, { useContext } from "react";
-import { Text } from "react-native";
 
+import { Text } from "../../../components/typography/text.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
 import { CartContext } from "../../../services/cart/cart.context";
 
 import { CreditCardInput } from "../components/credit-card.component";
 
+import { CartIconContainer, CartIcon } from "../components/checkout.styles";
+
 export const CheckoutScreen = () => {
   const { cart, restaurant } = useContext(CartContext);
+  if (!cart.length || !restaurant) {
+    return (
+      <SafeArea>
+        <CartIconContainer>
+          <CartIcon icon="cart-off" />
+          <Text>Your cart is empty!</Text>
+        </CartIconContainer>
+      </SafeArea>
+    );
+  }
   return (
     <SafeArea>
       <Text>{JSON.stringify(cart)}</Text>
