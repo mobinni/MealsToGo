491c2c69b8bfcbe2e5d26d614d48d30396d9a351
education <education@MacBook-Pro.local>
2021-01-15 23:50:09 -0500

    Checkout cart persistance

--- src/services/cart/cart.context.js
+++ src/services/cart/cart.context.js
@@ -13,6 +13,40 @@ export const CartContextProvider = ({ children }) => {
 
   const [sum, setSum] = useState(0);
 
+  const saveCart = async (rst, crt, uid) => {
+    try {
+      const jsonValue = JSON.stringify({ restaurant: rst, cart: crt });
+      await AsyncStorage.setItem(`@cart-${uid}`, jsonValue);
+    } catch (e) {
+      console.log("error storing", e);
+    }
+  };
+
+  const loadCart = async (uid) => {
+    try {
+      const value = await AsyncStorage.getItem(`@cart-${uid}`);
+      if (value !== null) {
+        const { restaurant: rst, cart: crt } = JSON.parse(value);
+        setRestaurant(rst);
+        setCart(crt);
+      }
+    } catch (e) {
+      console.log("error storing", e);
+    }
+  };
+
+  useEffect(() => {
+    if (user && user.uid) {
+      loadCart(user.uid);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (user && user.uid) {
+      saveCart(restaurant, cart, user.uid);
+    }
+  }, [restaurant, cart, user]);
+
   useEffect(() => {
     if (!cart.length) {
       setSum(0);
