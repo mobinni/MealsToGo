aecaac7d24859d2e078faef2b86013f9266d1ea8
Carson Rodrigues <rodriguescarson@gmail.com>
2021-05-25 21:38:09 +0530

    first commit

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+import styled from 'styled-components/native'
+
+const Title = styled.Text`
+padding:16px;
+color:red;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </Card>
+  )
+};
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+  title: { padding: 16 }
+})
