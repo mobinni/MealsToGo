a2e07b0bfed9eea7e83234c693cc40fdae199f4f
Jesse Lando F <orlandroide@gmail.com>
2023-01-10 21:55:09 +0000

    continue simplifying restaurant.screen.js

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { View, Text, StyleSheet, SafeAreaView, StatusBar } from "react-native";
 import { RestaurantsScreens } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
@@ -11,23 +8,3 @@ export default function App() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    // The next code line only applies to android
-    // For IOS status bar offset SafeAreaView is used
-    marginTop: StatusBar.currentHeight,
-    flex: 1,
-  },
-  search: {
-    backgroundColor: "#FBCC82",
-    padding: 10,
-  },
-  searchText: {},
-  list: {
-    flex: 1,
-    backgroundColor: "#FADD91",
-    padding: 10,
-  },
-  listText: {},
-});
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,8 +1,36 @@
 import React, { useContext, useState } from "react";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 import { Searchbar } from "react-native-paper";
 
 export const RestaurantsScreen = () => {
   return (
-   <SafeAreaView style={st}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar />
+      </View>
+      <View style={styles.list}>
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    // The next code line only applies to android
+    // For IOS status bar offset SafeAreaView is used
+    marginTop: StatusBar.currentHeight,
+    flex: 1,
+  },
+  search: {
+    backgroundColor: "#FBCC82",
+    padding: 10,
+  },
+  searchText: {},
+  list: {
+    flex: 1,
+    backgroundColor: "#FADD91",
+    padding: 10,
+  },
+  listText: {},
+});
