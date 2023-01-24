bb3ea52dba5dae94b54c92ba2a1ac9bcf024555c
lando <orlandroide@gmail.com>
2023-01-24 11:54:36 -0800

    work on restaurant global context

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -5,46 +5,13 @@ import {
   restaurantsTransform,
 } from "./restaurants.service";
 
-import { LocationContext } from "../location/location.context";
-
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
-  const [restaurants, setRestaurants] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const { location } = useContext(LocationContext);
-
-  const retrieveRestaurants = (loc) => {
-    setIsLoading(true);
-    setRestaurants([]);
-
-    restaurantsRequest(loc)
-      .then(restaurantsTransform)
-      .then((results) => {
-        setError(null);
-        setIsLoading(false);
-        setRestaurants(results);
-      })
-      .catch((err) => {
-        setRestaurants([]);
-        setIsLoading(false);
-        setError(err);
-      });
-  };
-  useEffect(() => {
-    if (location) {
-      const locationString = `${location.lat},${location.lng}`;
-      retrieveRestaurants(locationString);
-    }
-  }, [location]);
-
   return (
     <RestaurantsContext.Provider
       value={{
-        restaurants,
-        isLoading,
-        error,
+        restaurants: [],
       }}
     >
       {children}
