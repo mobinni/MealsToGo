22f100d7dbe67c6000445bb32ce18fce0f51e1ce
lando <orlandroide@gmail.com>
2023-01-25 17:12:36 -0800

    gather restaurants data

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -11,11 +11,30 @@ export const RestaurantsContextProvider = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout( () => {
+      restaurantsRequest().then(restaurantsTransform).then((results) => {
+        setIsLoading(false);
+        setRestaurants(results);
+    }).catch(err => {
+      setIsLoading(false);
+      setError(err);
+    };
+    }, 2000);
+  }
+  
+  //useEffect acts when something mounts or changes
+  useEffect(() => {
+retrieveRestaurants();
+  }, []); //use the [] so is not triggering all the time
 
   return (
     <RestaurantsContext.Provider
       value={{
-        restaurants: [1, 2, 3, 4, 5],
+        restaurants,
+        isLoading,
+        error,
       }}
     >
       {children}
